c921cd988455e4f3f76ca7d8dbc10d39ad3e859c
Sontsevolosa <sontsevolosa@gmail.com>
2024-12-20 19:23:40 +0200

    Long and short light sensor blinkings using a button

--- light_sensor_blinking.ino
+++ light_sensor_blinking.ino
@@ -1,31 +1,29 @@
-const int pinRed = 10;
-const int pinYellow = 11;
-const int pinGreen = 12;
+const int pinLed = 12;
+const int pinKey = 11;
+
+boolean mode = true;
+unsigned long lastTime = 0;
 
 void setup() {
-  pinMode(pinRed, OUTPUT);
-  pinMode(pinYellow, OUTPUT);
-  pinMode(pinGreen, OUTPUT);
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinKey, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(pinRed, HIGH);
-  digitalWrite(pinYellow, LOW);
-  digitalWrite(pinGreen, LOW);
-  delay(1500);
+  if (digitalRead(pinKey) == LOW) {
+    delay(500);
+    mode = !mode;
+  }
 
-  digitalWrite(pinRed, LOW);
-  digitalWrite(pinYellow, HIGH);
-  digitalWrite(pinGreen, LOW);
-  delay(1500);
-
-  digitalWrite(pinRed, LOW);
-  digitalWrite(pinYellow, LOW);
-  digitalWrite(pinGreen, HIGH);
-  delay(1500);
-
-  digitalWrite(pinRed, LOW);
-  digitalWrite(pinYellow, HIGH);
-  digitalWrite(pinGreen, LOW);
-  delay(1500);
+  if (mode) {
+    if (millis() - lastTime > 3000) {
+      lastTime = millis();
+      digitalWrite(pinLed, !digitalRead(pinLed));
+    }
+  } else {
+    digitalWrite(pinLed, HIGH);
+    delay(500);
+    digitalWrite(pinLed, LOW);
+    delay(500);
+  }
 }
